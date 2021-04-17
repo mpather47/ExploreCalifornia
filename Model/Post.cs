0aff6f9fa20854d82bf4ee361b9cc2b8a5f45e7e
mpather47 <mpather47@gmail.com>
2021-04-17 17:21:00 +0200

    first commit

---
+++ Model/Post.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Models
+{
+    public class Post
+    {
+        public long Id { get; set; }
+
+        public string _key;
+
+        public string Key
+        {
+            get
+            {
+                if (_key == null)
+                {
+                    _key = Regex.Replace(Title.ToLower(), "[^a-z0-9]", "-");
+                }
+                return _key;
+            }
+            set { _key = value; }
+        }
+
+        [Display(Name = "Post Title")]
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 5,
+            ErrorMessage = "Title must be between 5 and 100 characters long")]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        [Required]
+        
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+
+        public DateTime Posted { get; set; }
+    }
+}
